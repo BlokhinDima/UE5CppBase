651663c50f6f9f442569434a3cad0d4c34276365
Dmitry Blokhin <blokhindmitrijj@gmail.com>
2024-06-19 16:37:02 +0300

    Initial commit

---
+++ Source/CppBase/Public/CppBaseActor.h
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "CppBaseActor.generated.h"
+
+UCLASS()
+class CPPBASE_API ACppBaseActor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ACppBaseActor();
+
+	UPROPERTY(EditInstanceOnly)
+	int EnemyNum = 1;
+
+	UPROPERTY(EditInstanceOnly)
+	float CurrentHealth = 100;
+
+	UPROPERTY(EditInstanceOnly)
+	bool IsAlive = true;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+private:
+	void ShowActorInformation();
+
+};
